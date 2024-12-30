a8dac1af3f609c40c3b92757036597811c89fa9c
huudat31 <phanhuudat310708@gmail.com>
2024-12-30 07:47:56 +0700

    change file main.dart1

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,65 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const WeatherApp());
-}
+void main() => runApp(const MyApp());
 
-class WeatherApp extends StatelessWidget {
-  const WeatherApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(child: myWidget()),
-      ),
-      debugShowCheckedModeBanner: false,
+    return const MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
 
-class myWidget extends StatelessWidget {
-  const myWidget({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Text('Lol anh duy');
-  }
-}
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/background/background_01.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: const Stack(
+            children: [
+              Positioned(
+                top: 29.0,
+                left: 34.0,
+                child: Icon(
+                  Icons.menu,
+                  size: 40.0,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+              ),
+              Positioned(
+                top: 29.0,
+                left: 301.0,
+                child: Icon(
+                  Icons.search,
+                  size: 40.0,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+              ),
+              Positioned(
+                top: 60.0,
+                left: 12.0,
+                child: Image.asset(
+                  'assets/Group_07.png',
+                  width: 50.0,
+                  height: 50.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
